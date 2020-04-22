0b23a9e9269d057e58e49c0b51436602405828d4
Wilmer Aguilera <waguiler@redhat.com>
2020-04-22 06:40:14 -0500

    Ut

--- backend-users/src/test/java/com/redhat/example/DemoApplicationTests.java
+++ backend-users/src/test/java/com/redhat/example/DemoApplicationTests.java
@@ -2,15 +2,29 @@ package com.redhat.example;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.redhat.example.repository.UserRepository;
+import com.redhat.example.resource.UserResource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoApplicationTests {
+	
+	@Autowired
+	UserResource userResource;
 
 	@Test
 	void contextLoads() {
+		
+	}
+	
+	
+	@Test
+	void healthCheck() {
+		userResource.healthcheck();
 	}
 
 }

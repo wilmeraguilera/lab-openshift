d2088c0720ec93f5c3dacd9100db8d022650b5a5
Wilmer <wilmeraguilerab@gmail.com>
2020-05-13 16:48:31 -0500

    def

--- backend-users/JenkinsfileCopy.groovy
+++ backend-users/JenkinsfileCopy.groovy
@@ -78,30 +78,3 @@ pipeline {
         }
     }
 }
-
-def replaceValuesInFile(String templatePathFile, String finalPathFile, String propertiesPathFile) {
-    //Read properties file
-    Properties props = new Properties()
-    File propsFile = new File(propertiesPathFile)
-    props.load(propsFile.newDataInputStream())
-    Set values = props.entrySet()
-
-    //read template
-    File file = new File(templatePathFile)
-
-    //Copiar el contenido del template al archivo definitivo
-    Files.copy(file.toPath(), new File(finalPathFile).toPath(), StandardCopyOption.REPLACE_EXISTING)
-
-    def destFile = new File('/home/wilmeraguilera/Documents/application-dev.properties')
-
-    def content = destFile.text
-
-    values.each { property ->
-        content = content.replace('#{' + property.key + '}', property.value)
-    }
-
-    //Escribir archivo destino
-    destFile.text = content
-
-
-}

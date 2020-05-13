7595e567aa8dba31949dc54e95504eb46f6a4465
Wilmer <wilmeraguilerab@gmail.com>
2020-05-13 15:29:24 -0500

    Ajustes Jenkins File

--- backend-users/JenkinsfileCopy.groovy
+++ backend-users/JenkinsfileCopy.groovy
@@ -1,86 +1,90 @@
-pipeline{
-  agent any
+pipeline {
+    agent any
 
-  tools{
-    maven 'M2-3.6.3'
-    jdk 'JDK18'
-  }
-
-  stages{
-    stage("Checkout Source Code"){
-      steps{
-        echo "Checkout Source Code"
-        checkout scm
-      }
+    tools {
+        maven 'M2-3.6.3'
+        jdk 'JDK18'
     }
 
-    stage("Build"){
-      steps{
-        echo "Init Build"
-        //Only apply the next instruction if you have the code in a subdirectory
-        dir("backend-users"){
-          sh "mvn -Dmaven.test.skip=true compile"
+    stages {
+        stage("Checkout Source Code") {
+            steps {
+                echo "Checkout Source Code"
+                checkout scm
+            }
         }
-        echo "End Build"
-      }
-    }
 
-    stage("Unit Test"){
-      steps{
-        echo "Init Unit Test"
-        echo "End Unit Test"
-      }
-    }
+        stage("Build") {
+            steps {
+                echo "Init Build"
+                //Only apply the next instruction if you have the code in a subdirectory
+                dir("backend-users") {
+                    sh "mvn -Dmaven.test.skip=true compile"
+                }
+                echo "End Build"
+            }
+        }
 
-    stage("Execute Sonar analysis"){
-      steps{
-        echo "Execute Sonar analysis"
-      }
-    }
+        stage("Unit Test") {
+            steps {
+                echo "Init Unit Test"
+                echo "End Unit Test"
+            }
+        }
 
-    stage("Publish to Nexus"){
-      steps{
-        echo "Publish to Nexus"
-      }
-    }
+        stage("Execute Sonar analysis") {
+            steps {
+                echo "Execute Sonar analysis"
+            }
+        }
 
-    stage("Build Image"){
-      steps{
-        echo "Build Image"
-      }
-    }
+        stage("Publish to Nexus") {
+            steps {
+                echo "Publish to Nexus"
+            }
+        }
 
-    stage("Deploy DEV"){
-      steps{
-        echo "Deploy DEV"
-      }
+        stage("Build Image") {
+            steps {
+                echo "Build Image"
+            }
+        }
+
+        stage("Deploy DEV") {
+            steps {
+
+                dir('backend-users/src/main/resources'){
+                  replaceValuesInFile('application.properties','application-dev.properties','application-env.properties')
+                }
+                echo "Deploy DEV"
+            }
+        }
     }
-  }
 }
 
-def replaceValuesInFile (String templatePathFile, String finalPathFile, String propertiesPathFile){
-  //Read properties file
-  Properties props = new Properties()
-  File propsFile = new File(propertiesPathFile)
-  props.load(propsFile.newDataInputStream())
-  Set values = props.entrySet()
+def replaceValuesInFile(String templatePathFile, String finalPathFile, String propertiesPathFile) {
+    //Read properties file
+    Properties props = new Properties()
+    File propsFile = new File(propertiesPathFile)
+    props.load(propsFile.newDataInputStream())
+    Set values = props.entrySet()
 
-  //read template
-  File file = new File(templatePathFile)
+    //read template
+    File file = new File(templatePathFile)
 
-  //Copiar el contenido del template al archivo definitivo
-  Files.copy(file.toPath(), new File(finalPathFile).toPath(), StandardCopyOption.REPLACE_EXISTING)
+    //Copiar el contenido del template al archivo definitivo
+    Files.copy(file.toPath(), new File(finalPathFile).toPath(), StandardCopyOption.REPLACE_EXISTING)
 
-  def destFile = new File('/home/wilmeraguilera/Documents/application-dev.properties')
+    def destFile = new File('/home/wilmeraguilera/Documents/application-dev.properties')
 
-  def content = destFile.text
+    def content = destFile.text
 
-  values.each { property ->
-    content= content.replace('#{'+property.key+'}', property.value)
-  }
+    values.each { property ->
+        content = content.replace('#{' + property.key + '}', property.value)
+    }
 
-  //Escribir archivo destino
-  destFile.text = content
+    //Escribir archivo destino
+    destFile.text = content
 
 
 }

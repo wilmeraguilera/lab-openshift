78485b65a103983b5398601bba388f925218ff90
Wilmer <wilmeraguilerab@gmail.com>
2020-05-13 15:16:12 -0500

    Ajustes Jenkins File

--- backend-users/JenkinsfileCopy.groovy
+++ backend-users/JenkinsfileCopy.groovy
@@ -1,6 +1,9 @@
 pipeline{
-
   agent any
+
+  enviroment{
+
+  }
 
   tools{
     maven 'M2-3.6.3'
@@ -17,16 +20,19 @@ pipeline{
 
     stage("Build"){
       steps{
-        echo "Build"
+        echo "Init Build"
+        //Only apply the next instruction if you have the code in a subdirectory
         dir("backend-users"){
           sh "mvn -Dmaven.test.skip=true compile"
         }
+        echo "End Build"
       }
     }
 
     stage("Unit Test"){
       steps{
-        echo "Unit Test"
+        echo "Init Unit Test"
+        echo "End Unit Test"
       }
     }
 
@@ -54,4 +60,32 @@ pipeline{
       }
     }
   }
+}
+
+def replaceValuesInFile (String templatePathFile, String finalPathFile, String propertiesPathFile){
+  //Read properties file
+  Properties props = new Properties()
+  File propsFile = new File(propertiesPathFile)
+  props.load(propsFile.newDataInputStream())
+  Set values = props.entrySet()
+
+  //read template
+  File file = new File(templatePathFile)
+
+  //Copiar el contenido del template al archivo definitivo
+  Files.copy(file.toPath(), new File(finalPathFile).toPath(), StandardCopyOption.REPLACE_EXISTING)
+
+  def destFile = new File('/home/wilmeraguilera/Documents/application-dev.properties')
+
+  def content = destFile.text
+
+  values.each { property ->  {
+    content= content.replace('#{'+property.key+'}', property.value)
+  }
+  }
+
+  //Escribir archivo destino
+  destFile.text = content
+
+
 }

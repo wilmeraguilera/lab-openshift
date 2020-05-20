0527105931aab4e220569d94a3f10ed697f49525
Wilmer <wilmeraguilerab@gmail.com>
2020-05-19 19:02:37 -0500

    Test jdk agent

--- backend-users/Jenkinsfile-plugin.groovy
+++ backend-users/Jenkinsfile-plugin.groovy
@@ -33,9 +33,10 @@ pipeline {
         echo "Init Checkout Source Code"
         checkout scm
         script {
+          sh 'java -version'
           env.JAVA_HOME="${tool 'JDK18'}"
           env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
-          sh 'java -version'
+
           echo "PATH is: $PATH"
           echo "JAVA is: $JAVA_HOME"
           echo "Path: ${PATH}"

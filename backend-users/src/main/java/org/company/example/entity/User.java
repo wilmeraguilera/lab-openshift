b23b4d2062dbc64396516ddbb6354b95098035ee
Wilmer <wilmeraguilerab@gmail.com>
2020-07-10 11:18:44 -0500

    Documentacion lab

--- backend-users/src/main/java/org/company/example/entity/User.java
+++ backend-users/src/main/java/org/company/example/entity/User.java
@@ -2,13 +2,14 @@ package org.company.example.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String email;

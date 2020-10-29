0cca5722bb796a67341df2697920cc25d44c55a9
Wilmer <wilmeraguilerab@gmail.com>
2020-10-29 11:21:50 -0500

    Test Pipeline

---
+++ labs/anchore.sh
@@ -0,0 +1,6 @@
+export ANCHORE_CLI_URL=http://my-route-openshift-operators.apps.cluster-59bf.59bf.sandbox902.opentlc.com/v1
+export ANCHORE_CLI_USER=admin
+export ANCHORE_CLI_PASS=foobar
+
+##list Images
+anchore-cli image list

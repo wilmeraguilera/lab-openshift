47073588566edd7c66782f0ed1829c57cca62e44
Wilmer <wilmeraguilerab@gmail.com>
2020-11-23 09:19:03 -0500

    Error

--- backend-users/src/main/java/org/company/example/resource/UserResource.java
+++ backend-users/src/main/java/org/company/example/resource/UserResource.java
@@ -33,7 +33,7 @@ public class UserResource {
 	private Logger logger = LoggerFactory.getLogger(UserResource.class);
 	
 	@Autowired
-	private UserRepository userRepository;
+	private UserRepository1 userRepository;
 	
 	@Value("${app.title}")
 	private String appTitle;
